3114357d51e0d35938fba8768b4497fbce94e808
raphael1233333 <cod1raph@protonmail.com>
2026-01-17 07:46:40 +0100

    first commit

---
+++ src/client/components/view.cpp
@@ -0,0 +1,62 @@
+#include "pch.h"
+#if 1
+#include "view.h"
+
+namespace view
+{
+	stock::cvar_t* cg_fovScaleEnable;
+	stock::cvar_t* cg_fovScale;
+	stock::cvar_t* record_respawn;
+
+	utils::hook::detour hook_CG_Respawn;
+	utils::hook::detour hook_CL_PlayDemo_f;
+	
+	static float scaledFOV(float fov)
+	{
+		int* flag = (int*)ABSOLUTE_CGAME_MP(0x302071dc); // Might be cg.snap->ps.eFlags
+		if (*flag & stock::EF_MG42_ACTIVE)
+			return 55;
+
+		if (cg_fovScaleEnable->integer)
+			return fov * cg_fovScale->value;
+
+		return fov;
+	}
+	
+	static __declspec(naked) void stub_CG_CalcFov_return()
+	{
+		__asm
+		{
+			sub esp, 0x4;
+			fstp dword ptr[esp];			
+			push dword ptr[esp];
+			call scaledFOV;
+			add esp, 0x4;
+
+			fstp dword ptr[esp];
+			fld dword ptr[esp];
+			add esp, 0x4;
+
+			pop ecx;
+			ret;
+		}
+	}
+	
+	class component final : public component_interface
+	{
+	public:
+		void post_unpack() override
+		{
+			cg_fovScaleEnable = stock::Cvar_Get("cg_fovScaleEnable", "0", stock::CVAR_ARCHIVE);
+			cg_fovScale = stock::Cvar_Get("cg_fovScale", "1", stock::CVAR_ARCHIVE);
+		}
+
+		void post_cgame() override
+		{
+			utils::hook::jump(ABSOLUTE_CGAME_MP(0x30032f2a), stub_CG_CalcFov_return);
+		}
+	};
+}
+
+REGISTER_COMPONENT(view::component)
+#endif
---
+++ src/client/components/view.h
@@ -0,0 +1,13 @@
+#include "shared.h"
+
+#include "hook.h"
+
+#include "loader/component_loader.h"
+
+#include "scheduler.h"
+
+namespace view
+{
+	extern stock::cvar_t* cg_fovScaleEnable;
+	extern stock::cvar_t* cg_fovScale;
+}
